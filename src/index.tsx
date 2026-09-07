0043601e51c7180cccb01883e4d147453ac5320d
Jennifer Adams <jennadamscodes@gmail.com>
2026-09-07 19:54:30 -0400

    Install Montserrat font

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import * as React from "react";
 import { StrictMode } from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App.js";
+import '@fontsource-variable/montserrat/wght.css';
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App/></StrictMode>)
