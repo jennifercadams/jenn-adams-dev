e0007b6c627d04918d962deae0548cdae2236d22
Jennifer Adams <jennadamscodes@gmail.com>
2026-09-08 22:24:15 -0400

    Create page layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "~pages/HomePage/HomePage.js";
+import Page from "~pages/Page/Page.js";
 import "./App.css";
 
 export default () => (
     <BrowserRouter>
         <Routes>
-            <Route index element={<HomePage />} />
+            <Route element={<Page />}>
+                <Route index element={<HomePage />} />
+            </Route>
         </Routes>
     </BrowserRouter>
 );
---
+++ src/pages/Page/Page.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { NavLink, Outlet } from "react-router";
+import "./Page.css";
+
+const Page = () => {
+    return (
+        <div id="main">
+            <div id="header">
+                <div id="title">Jenn Adams</div>
+                <nav id="nav">
+                    <NavLink className="nav-link" to="/">Home</NavLink>
+                    <NavLink className="nav-link" to="/About">About</NavLink>
+                    <NavLink className="nav-link" to="/Projects">Projects</NavLink>
+                    <NavLink className="nav-link" to="/Contact">Contact</NavLink>
+                </nav>
+            </div>
+            <Outlet />
+        </div>
+    );
+}
+
+export default Page;
