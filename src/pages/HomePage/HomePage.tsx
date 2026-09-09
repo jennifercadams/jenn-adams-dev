3c97be5b5c702aae2150a9511b3fe7e28094b63f
Jennifer Adams <jennadamscodes@gmail.com>
2026-09-09 10:02:05 -0400

    Refine HomePage css

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     return (
         <div id="home">
             <div id="intro" className="content">
-                <p>Hello, my name is</p>
+                <p className="small-text">Hello, my name is</p>
                 <h1>Jenn Adams</h1>
                 <p>I'm a software engineer specializing in full stack web development.</p>
             </div>
