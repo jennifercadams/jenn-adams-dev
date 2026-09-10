1c421fa80c0bd2fec170006c636849ad9484a9cf
Jennifer Adams <jennadamscodes@gmail.com>
2026-09-10 13:37:39 -0400

    Change image on About page

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
+import { Link } from "react-router";
 import "./AboutPage.css";
 
 const AboutPage = () => {
     return (
         <div id="about">
-            <div id="me">
+            <div id="text">
                 <h1>About Me</h1>
                 <p>I'm a software engineer with over four years of professional experience in full stack web development working primarily with .NET, Angular, and React. I have experience maintaining complex web applications as well as implementing new features. I have taken the lead on numerous feature projects and have completed integrations with third-party APIs. I also have experience building and maintaining automated test suites with Selenium and working with the PowerShell scripts that automate setting up, running tests, and recording results.</p>
                 <p>I have a passion for building things, especially things that bring people joy. I get excited about new technologies and putting them to work to make fun projects, and I think this is the best and most rewarding way to learn something new. I also take pride in thoughtful architecture and concise, maintainable code.</p>
                 <p>In my personal life, I enjoy reading, writing, video games, TTRPGs, baking, and spending time with my dog Wednesday.</p>
             </div>
-            <div id="wednesday">
-                <img src="./src/assets/images/Wednesday.jpg" />
+            <div id="image">
+                <img src="./src/assets/images/code-speech-bubble.svg" />
             </div>
         </div>
     );
