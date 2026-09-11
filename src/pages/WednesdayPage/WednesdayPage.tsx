87fda1cd97ccce85e4980382a314917398f70a37
Jennifer Adams <jennadamscodes@gmail.com>
2026-09-10 21:51:00 -0400

    Add Wednesday gallery

--- src/pages/WednesdayPage/WednesdayPage.tsx
+++ src/pages/WednesdayPage/WednesdayPage.tsx
@@ -1,9 +1,28 @@
 import * as React from "react";
+import useWednesdayPage from "./useWednesdayPage.js";
 import "./WednesdayPage.css";
 
 const WednesdayPage = () => {
+    const {
+        index,
+        currentImagePath,
+        gallerySize,
+        handleClickPrev,
+        handleClickNext
+    } = useWednesdayPage();
+
     return (
-        <div id="wednesday"></div>
+        <div id="wednesday">
+            <div id="gallery">
+                <div id="image">
+                    <img id="current" src={currentImagePath} />
+                </div>
+                <div id="buttons">
+                    <button id="prev" onClick={handleClickPrev} disabled={index == 0}>{"<"}</button>
+                    <button id="next" onClick={handleClickNext} disabled={index == gallerySize - 1}>{">"}</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
---
+++ src/pages/WednesdayPage/useWednesdayPage.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+const gallerySize: number = 26;
+const imagePaths: string[]  = [];
+for (let i = 1; i <= gallerySize; i++) {
+    var path = `./src/assets/images/Wednesday${i.toString().padStart(2, "0")}.jpg`;
+    imagePaths.push(path);
+};
+
+const useWednesdayPage = () => {
+    const [ index, setIndex ] = useState<number>(0);
+    const [ currentImagePath, setCurrentImagePath ] = useState<string>(imagePaths[0]);
+
+    useEffect(() => {
+        setCurrentImagePath(imagePaths[index]);
+    }, [index]);
+
+    const handleClickPrev = () => {
+        if (index > 0) {
+            setIndex(index - 1);
+        }
+    };
+
+    const handleClickNext = () => {
+        if (index < gallerySize - 1) {
+            setIndex(index + 1);
+        }
+    };
+
+    return {
+        index,
+        currentImagePath,
+        gallerySize,
+        handleClickPrev,
+        handleClickNext
+    };
+};
+
+export default useWednesdayPage;
