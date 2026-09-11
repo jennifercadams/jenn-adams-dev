2b87c9b6a0e645b913d859c8c5af56c40201f7f2
Jennifer Adams <jennadamscodes@gmail.com>
2026-09-11 19:48:01 -0400

    Add page for Lander Game

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AboutPage from "~pages/AboutPage/AboutPage.js";
 import HomePage from "~pages/HomePage/HomePage.js";
 import Page from "~pages/Page/Page.js";
 import ProjectsPage from "~pages/ProjectsPage/ProjectsPage.js";
+import ProjectWrapper from "~components/ProjectWrapper/ProjectWrapper.js";
 import WednesdayPage from "~pages/WednesdayPage/WednesdayPage.js";
 import "./App.css";
 
@@ -14,6 +15,13 @@ export default () => (
                 <Route index element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
                 <Route path="/projects" element={<ProjectsPage />} />
+                <Route path="/projects/lander-game" element={
+                    <ProjectWrapper
+                        title="Lander Game" 
+                        htmlSrc="./../src/projects/LanderGame/LanderGame.html" 
+                        cssSrc="./../src/projects/LanderGame/LanderGame.css"
+                    />
+                }/>
             </Route>
             <Route element={<Page title="Wednesday Adams" />}>
                 <Route path="/wednesday" element={<WednesdayPage />} />
