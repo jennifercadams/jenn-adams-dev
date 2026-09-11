0e90ba4eda35c7a0b69f99401cf2ca183a4dd5d0
Jennifer Adams <jennadamscodes@gmail.com>
2026-09-11 01:59:15 -0400

    Add connect links to Home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -10,6 +10,23 @@ const HomePage = () => {
                 <p className="small-text">Hello, my name is</p>
                 <p className="large-text">Jenn Adams</p>
                 <p className="medium-text">I'm a software engineer specializing in full stack web development.</p>
+                <div id="connect">
+                    <p className="medium-text">
+                        <span id="arrow">{">"}</span>
+                        Let's connect.
+                    </p>
+                    <div id="icon-container">
+                        <a href="https://www.linkedin.com/in/jenn-adams-dev/" target="_blank" rel="noopener noreferrer">
+                            <img className="icon" src="./src/assets/icons/linkedin.png" />
+                        </a>
+                        <a href="https://github.com/jennifercadams/" target="_blank" rel="noopener noreferrer">
+                            <img className="icon" src="./src/assets/icons/github.png" />
+                        </a>
+                        <a href="mailto:contact@jennadams.dev">
+                            <img className="icon" src="./src/assets/icons/email.png" />
+                        </a>
+                    </div>
+                </div>
             </div>
             <div id="skills" className="content">
                 {TechIconList.map((props, index) => {
