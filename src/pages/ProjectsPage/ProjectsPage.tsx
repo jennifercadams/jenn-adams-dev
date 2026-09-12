79ff66273f69573825fd79329da3b463b076e678
Jennifer Adams <jennadamscodes@gmail.com>
2026-09-11 22:32:34 -0400

    Add LanderGame card and style projects page

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,9 +1,18 @@
 import * as React from "react";
+import ProjectCard from "~components/ProjectCard/ProjectCard.js";
 import "./ProjectsPage.css";
 
 const ProjectsPage = () => {
     return (
-        <div id="projects"></div>
+        <div id="projects">
+            <ProjectCard
+                imgSrc="./src/assets/images/LanderGameTitle.jpg"
+                title="Lander Game"
+                description="A simple Lunar Lander clone built in Unity with procedural 2D terrain generation."
+                action="Play"
+                actionPath="/projects/lander-game"
+            />
+        </div>
     );
 };
 
